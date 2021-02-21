a2280bcc977becd9603fa5df9570b940b14f5f2a
Marcus Hanson <mah.dvlpr@gmail.com>
2021-02-21 15:53:51 +0100

    Updated examples and minor changes to animation.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,18 @@ import 'dart:io';
 import 'package:cli_goodies/cli_goodies.dart';
 
 void main() async {
-    await animationStart();
-    sleep(Duration(seconds: 10)); // Emulating demanding computation.
-    animationStop();
+  print('Starting computation 1...');
+  await animationStart();
+  sleep(Duration(seconds: 2)); // Emulating demanding computation.
+  animationStop();
+
+  print('Starting computation 2...');
+  await animationStart();
+  sleep(Duration(seconds: 4)); // Emulating demanding computation.
+  animationStop();
+
+  print('Starting computation 3...');
+  await animationStart();
+  sleep(Duration(seconds: 8)); // Emulating demanding computation.
+  animationStop();
   }
--- lib/src/animate.dart
+++ lib/src/animate.dart
@@ -2,18 +2,25 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:isolate';
 
+/// Active isolate object. Is created when [animationStart()] is called.
 Isolate _animationIsolate;
 
+/// Starts an animation on a new line.
 Future<Isolate> animationStart() async {
+  stdout.write('\n');
   return _animationIsolate ?? (_animationIsolate = await Isolate.spawn(_animate, null));
 }
 
+/// Stops current animation by killing the isolate instance.
 void animationStop() {
   _animationIsolate.kill(priority: Isolate.immediate);
   _animationIsolate = null;
-  stdout.write('\n');
+  stdout.write('\x1B[2K\x1B[1G');
 }
 
+/// Run the animation of active [_animationIsolate].
+/// 
+/// The [sp] is ignored, and can be set to null.
 Future<void> _animate(SendPort sp) async {
   var frames = const <String>[
       '⠁',
